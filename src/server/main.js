146e577ee18024ab53e661295d6042af54b6faea
Brian Ressler <carlofabyss@hotmail.com>
2025-06-19 12:10:38 -0500

    Let's keep the boilerplate code on server for now, I am thinking of moving to socket.io

--- src/server/main.js
+++ src/server/main.js
@@ -1,6 +1,5 @@
 import express from "express";
 import ViteExpress from "vite-express";
-import WebSocket, { WebSocketServer } from 'ws';
 import https from 'https';
 import fs from 'fs';
 
@@ -10,37 +9,7 @@ const keyPath = 'privkey.pem';
 const mode = "development";
 const app = express();
 
-// Create a WebSocket server
-const wss = new WebSocketServer({ port: 5000 }); // You can choose a different port
-
-// Store chat messages in memory
 const messages = [];
-
-wss.on('connection', ws => {
-  console.log('Client connected');
-
-  // Send all previous messages to the new client
-  messages.forEach(msg => {
-    ws.send(msg);
-  });
-
-  ws.on('message', message => {
-    console.log(`Received message: ${message}`);
-    // Store the message
-    messages.push(message.toString());
-    // Broadcast to all connected clients
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
-
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
-
-});
 
 try {
   ViteExpress.config({
