94325169167406bb4405d9ee822c289306a10652
Brian Ressler <carlofabyss@hotmail.com>
2025-06-19 16:44:13 -0500

    Get some kind of UI going now that our basic functionality seems stable

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef, use } from "react";
 import "./App.css";
 import Bar from "./components/Bar/Bar.jsx";
 import io from 'socket.io-client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const socket = io();
 console.log("Socket initialized");
@@ -11,6 +12,44 @@ function App() {
   const [messages, setMessages] = useState(initialMessages);
   const socketRef = useRef(null);
   const [initialized, setInitialized] = useState(false);
+
+  const theme = createTheme({
+      typography: {
+        h3: {
+          fontWeight: 700,
+        }
+      },
+      components: {
+        MuiButton: {
+          defaultProps: {
+            size: 'medium'
+          }
+        },
+        MuiTextField: {
+          defaultProps: {
+            size: 'small'
+          }
+        },
+      },
+      palette: {
+        mode: 'dark',
+        primary: {
+          main: '#d83e00',
+          light: '#e54100',
+          dark: '#FFF',
+          contrastText: '#FFF',
+        },
+        secondary: {
+          main: '#0d425f',
+          light: '#0f4b6b',
+          dark: '#0a3045',
+          contrastText: '#FFF',
+        },
+        accent: {
+          main: '#1e8806',
+        }
+      },
+    });
 
   if(initialized == false) {
     setInitialized(true);
@@ -48,12 +87,14 @@ function App() {
   };
 
   return (
-    <div className="App">
-      {messages.map((message, index) => (
-          <li>{message.text}</li>
-      ))}
-      <Bar handleSend={handleSend}/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        {messages.map((message, index) => (
+            <li>{message.text}</li>
+        ))}
+        <Bar handleSend={handleSend}/>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/client/components/Bar/Bar.jsx
+++ src/client/components/Bar/Bar.jsx
@@ -3,6 +3,7 @@ import './Bar.css';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 function Bar({handleSend}) {
     const inputRef = useRef(null);
@@ -14,12 +15,12 @@ function Bar({handleSend}) {
     }
 
     return ( 
-    <div className='app-bar'>
-        <FormControl>
-            <TextField ref={inputRef} id="outlined-basic" label="Outlined" variant="outlined" />
-            <Button onClick={() => { emitMessage()}} variant="contained">Contained</Button>
+    <Box className='app-bar'>
+        <FormControl className='app-form'>
+            <Box className="app-input" sx={{ p: 2 }}><TextField className='app-input-control' ref={inputRef} id="outlined-basic" label="Outlined" variant="outlined" /></Box>
+            <Box className="app-options" sx={{ p: 2 }}><Button className='app-options-send-opt' onClick={() => { emitMessage()}} variant="contained">Contained</Button></Box>
         </FormControl>
-    </div>);
+    </Box>);
 }
 
 export default Bar;
